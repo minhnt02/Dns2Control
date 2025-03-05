f1405a24392c6d3161b7ff1ca46796eaba575380
NguyenTranMinh <111475828+minhnt02@users.noreply.github.com>
2025-03-05 12:27:50 +0700

    Update capture.py

--- capture.py
+++ capture.py
@@ -2,9 +2,7 @@ from scapy.all import sniff, DNSQR
 
 def process_dns_packet(packet):
     if packet.haslayer(DNSQR):
-        # Lấy thông tin từ gói tin DNS
         dns_query = packet[DNSQR].qname.decode('utf-8')
         print(f"DNS Query: {dns_query}")
 
-# Bắt các gói tin DNS
 sniff(filter="udp port 53", prn=process_dns_packet)
